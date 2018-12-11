0853912ecbd1259e3fc23b13415825a06905d249
Ronnel <ronnelronnelronnel216@gmail.com>
2018-12-11 10:16:24 +0900

    プロジェクト機能の拡張 コードエディタの自動起動

---
+++ Assets/ProjectSettings/ProjectExtension.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using UnityEditor;
+using UnityEngine.Events;
+
+[InitializeOnLoad]
+public static class EditorApplicationUtility
+{
+    private const BindingFlags BINDING_ATTR = BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic;
+
+    private static readonly FieldInfo m_info = typeof(EditorApplication).GetField("projectWasLoaded", BINDING_ATTR);
+
+    public static UnityAction projectWasLoaded
+    {
+        get
+        {
+            return m_info.GetValue(null) as UnityAction;
+        }
+        set
+        {
+            var functions = m_info.GetValue(null) as UnityAction;
+            functions += value;
+            m_info.SetValue(null, functions);
+        }
+    }
+}
+
+public static class Example
+{
+    [InitializeOnLoadMethod]
+    private static void Hoge()
+    {
+        EditorApplicationUtility.projectWasLoaded += () =>
+        {
+            EditorApplication.ExecuteMenuItem("Assets/Open C# Project");
+        };
+    }
+}
