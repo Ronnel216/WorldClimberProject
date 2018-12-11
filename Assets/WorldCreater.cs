e1e7f0ff2880ba1691f47bd1e63441204d2443a0
Ronnel <ronnelronnelronnel216@gmail.com>
2018-12-11 14:53:41 +0900

    メッシュ生成　保存 生成メッシュに　paintツールの使用テスト

--- Assets/WorldCreater.cs
+++ Assets/WorldCreater.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using System;
 
 // 必須のコンポーネント
 [RequireComponent(typeof(MeshRenderer))]
@@ -8,16 +10,28 @@ using UnityEngine;
 
 public class WorldCreater : MonoBehaviour {
 
+    // 一面あたりに必要な頂点指定数
+    /* 例
+     * 0,1,
+     * 2,3
+     * triangles = {0, 1, 2, 1, 3, 2} 
+     */
+    const int TrianglesNumPerFace = 6;
 
-    // 面のサイズ
+    // 地形のポリゴン数　面数
     [SerializeField]
-    Vector2Int size = new Vector2Int(10, 5);
+    Vector2Int polyNum = new Vector2Int(10, 5);
+    // 頂点数
+    Vector2Int vertNum;
 
     // Use this for initialization
     void Start () {
+        // 面数に必要な分だけ確保
+        vertNum = new Vector2Int(polyNum.x + 1, polyNum.y + 1);
+
         var filter = GetComponent<MeshFilter>();
         filter.sharedMesh = CreateMesh();
-
+        AssetDatabase.CreateAsset(filter.sharedMesh, "Assets/Temp/"+ filter.sharedMesh.name +".asset");
     }
 	
 	// Update is called once per frame
@@ -25,35 +39,38 @@ public class WorldCreater : MonoBehaviour {
 		
 	}
 
+    
+
     /// <summary>
     /// メッシュの生成
     /// </summary>
     /// <returns></returns>
     Mesh CreateMesh()
     {
-        // 面数に必要な分だけ確保
-        Vector3[] vertices = new Vector3[(size.x + 1) * (size.y + 1)];
-        int[] triangles = new int[(size.x * size.y) * 6];
-        Debug.Log("vert " + vertices.Length);
-        Debug.Log("tri " + triangles.Length);
-        // メッシュデータの生成
-        for (int i = 0; i < vertices.Length; i++)
-        {
-            vertices[i] = new Vector3(i % (size.x + 1), -i / (size.x + 1));
-            Debug.Log("vert " + vertices[i]);
+        // メッシュ設定データ
+        Vector3[] vertices = new Vector3[vertNum.x * vertNum.y];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[(polyNum.x * polyNum.y) * TrianglesNumPerFace];
 
-        }
+        // メッシュデータの生成 -----
 
-        for (int i = 0; i < size.x * size.y; i++)
+        // 頂点の配置 地形の形作り
+        ShapeLandShape(ref vertices, ref uv);
+
+        // 頂点インデックスの割り当て
+        for (int i = 0; i < polyNum.x * polyNum.y; i++)
         {
+            // ポリゴン 面の番号
+            int polyNo = (i % polyNum.x) * TrianglesNumPerFace + (i / polyNum.x) * (polyNum.x * TrianglesNumPerFace);
+
+            // 頂点インデックス計算用
             /*
              * x.x + y.x, x.y + y.x,
              * x.x + y.y, x.y + y.y
              */
+            Vector2Int x = new Vector2Int((i % polyNum.x), (i % polyNum.x) + 1);
+            Vector2Int y = new Vector2Int((i / polyNum.x) * (polyNum.x + 1), (i / polyNum.x) * vertNum.x + vertNum.x);
 
-            int polyNo = (i % size.x) * 6 + (i / size.x) * (size.x * 6);
-            Vector2Int x = new Vector2Int((i % size.x), i % size.x + 1);
-            Vector2Int y = new Vector2Int((i / size.x) * (size.x + 1), (i / size.x) * (size.x + 1) + (size.x + 1));
             triangles[polyNo]     = x.x + y.x;
             triangles[polyNo + 1] = x.y + y.x;
             triangles[polyNo + 2] = x.x + y.y;
@@ -62,24 +79,44 @@ public class WorldCreater : MonoBehaviour {
             triangles[polyNo + 4] = x.y + y.y;
             triangles[polyNo + 5] = x.x + y.y;
 
-
-            Debug.Log("triNo" + (polyNo + 0).ToString() + " : "+ triangles[polyNo]     );
-            Debug.Log("triNo" + (polyNo + 1).ToString() + " : "+ triangles[polyNo + 1] );
-            Debug.Log("triNo" + (polyNo + 2).ToString() + " : "+ triangles[polyNo + 2] );
-
-            Debug.Log("triNo" + (polyNo + 3).ToString() + " : "+ triangles[polyNo + 3] );
-            Debug.Log("triNo" + (polyNo + 4).ToString() + " : "+ triangles[polyNo + 4] );
-            Debug.Log("triNo" + (polyNo + 5).ToString() + " : "+ triangles[polyNo + 5]);
-
         }
+
+        // 生成したデータの設定 ---
 
         Mesh mesh = new Mesh();
         mesh.name = "TestMesh";
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.uv = uv;
 
+        // 再計算
         mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+        mesh.RecalculateBounds();
 
         return mesh;
+    }
+
+    private void ShapeLandShape(ref Vector3[] vertices, ref Vector2[] uv)
+    {
+        // 頂点の配置
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            float distance = 1;
+
+            vertices[i] = new Vector3(i % vertNum.x * distance, i / vertNum.x * -distance);
+
+            vertices[i] -= new Vector3(vertNum.x / 2.0f, vertNum.y * -distance, 0);     // 仮のメッシュのOffset
+        }
+
+        // uvの設定
+        // ! 面の数は　x > 1 && y > 1とする
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            float distance = 1.0f / (vertNum.x - 1);
+            Vector2 point = new Vector2((float)(i % vertNum.x) / (vertNum.x - 1), (float)(i / vertNum.x) / (vertNum.y - 1));
+
+            uv[i] = point;
+        }
     }
 }
