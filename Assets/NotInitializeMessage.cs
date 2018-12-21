625eb5130e9809b004806ead5611969e2d9ec177
Ronnel <ronnelronnelronnel216@gmail.com>
2018-12-21 16:29:09 +0900

    Nullチェック

---
+++ Assets/NotInitializeMessage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 未初期化　警告用
+/// </summary>
+public class NotInitializeMessage : MonoBehaviour {
+
+    public static GameObject Create(string message)
+    {
+        var obj = new GameObject();
+        obj.AddComponent<NotInitializeMessage>();
+        obj.name = message;
+        return obj;
+    }
+
+    void Awake()
+    {
+        Debug.Log("NotInitialize : " + gameObject.name);
+        Debug.Break();
+    }
+}
