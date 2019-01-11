02aa02a0b2bb22f41a9d399f8544ed0ccb06f473
Ronnel <ronnelronnelronnel216@gmail.com>
2019-01-11 15:51:46 +0900

    エディタ終了時にsourcetreeを自動起動

--- Assets/ProjectSettings/ProjectExtension.cs
+++ Assets/ProjectSettings/ProjectExtension.cs
@@ -7,31 +7,57 @@ public static class EditorApplicationUtility
 {
     private const BindingFlags BINDING_ATTR = BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic;
 
-    private static readonly FieldInfo m_info = typeof(EditorApplication).GetField("projectWasLoaded", BINDING_ATTR);
+    private static readonly FieldInfo m_info0 = typeof(EditorApplication).GetField("projectWasLoaded", BINDING_ATTR);
+    private static readonly FieldInfo m_info1 = typeof(EditorApplication).GetField("editorApplicationQuit", BINDING_ATTR);
 
     public static UnityAction projectWasLoaded
     {
         get
         {
-            return m_info.GetValue(null) as UnityAction;
+            return m_info0.GetValue(null) as UnityAction;
         }
         set
         {
-            var functions = m_info.GetValue(null) as UnityAction;
+            var functions = m_info0.GetValue(null) as UnityAction;
             functions += value;
-            m_info.SetValue(null, functions);
+            m_info0.SetValue(null, functions);
+        }
+    }
+
+    public static UnityAction editorApplicationQuit
+    {
+        get
+        {
+            return m_info1.GetValue(null) as UnityAction;
+        }
+        set
+        {
+            var functions = m_info1.GetValue(null) as UnityAction;
+            functions += value;
+            m_info1.SetValue(null, functions);
         }
     }
 }
 
-public static class Example
+public static class EditorCallBackMethod
 {
     [InitializeOnLoadMethod]
-    private static void Hoge()
+    private static void AddEventProjectWasLoaded()
     {
         EditorApplicationUtility.projectWasLoaded += () =>
         {
             EditorApplication.ExecuteMenuItem("Assets/Open C# Project");
+        };
+
+
+    }
+
+    [InitializeOnLoadMethod]
+    private static void AddEventEditorApplicationQuit()
+    {
+        EditorApplicationUtility.editorApplicationQuit += () =>
+        {
+            System.Diagnostics.Process.Start("C:/Users/s162121/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Atlassian/Sourcetree");
         };
     }
 }
