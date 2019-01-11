db2e771155c1996ec3db3d4fe04b840e273c0fa0
Ronnel <ronnelronnelronnel216@gmail.com>
2019-01-11 12:54:30 +0900

    3dベクトルのデバッグ表示用

---
+++ Assets/DebugVisualizeVector3.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugVisualizeVector3 : MonoBehaviour {
+
+    static public Vector3 visualize;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, visualize);
+	}
+}
