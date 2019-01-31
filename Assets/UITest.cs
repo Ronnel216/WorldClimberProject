f98bbc5d9aef988bf1c0151f3b2875105c25d28b
Ronnel <ronnelronnelronnel216@gmail.com>
2019-01-31 15:12:26 +0900

    UIのテスト

---
+++ Assets/UITest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITest : MonoBehaviour {
+
+    public void Display(float n)
+    {
+        Debug.Log(n);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
